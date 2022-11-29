2ad47bc5493ccdc349a1032c7908afd83f233e67
Isaac Moretão <91755254+IsaacMoretao@users.noreply.github.com>
2022-11-29 13:11:44 -0300

    Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,31 @@ export default function Home() {
   })
 
   return (
-    <div className="h-screen w-screen bg-black flex flex-col gap-4 items-end justify-end p-8">
-      <section className="border border-white-500 p-14 bg-#ffffff24">{data?.message}</section>
+    <>
+      <div className='absolute flex justify-center items-center w-full h-full'>
+        <section
+          className="border border-white-500
+          p-14 rounded-md
+          text-white font-bold">
+            {data?.message}
+        </section>
+      </div>
+      <div className="h-screen w-screen bg-black flex flex-col gap-4 items-end justify-end p-8">
 
-      <Popover.Root>
-        <Popover.Portal>
-          <Popover.Content align="end" sideOffset={16}>
-            <WidgetForm />
-          </Popover.Content>
-        </Popover.Portal>
 
-        <Popover.Trigger className="bg-violet-500 rounded-full h-12 w-12 flex items-center justify-center hover:bg-violet-600">
-          <Chat className="text-white h-5 w-5" weight="bold" />
-        </Popover.Trigger>
-      </Popover.Root>
-    </div>
+        <Popover.Root>
+          <Popover.Portal>
+            <Popover.Content align="end" sideOffset={16}>
+              <WidgetForm />
+            </Popover.Content>
+          </Popover.Portal>
+
+          <Popover.Trigger
+          className="bg-violet-500 rounded-full h-12 w-12 flex items-center justify-center hover:bg-violet-600">
+            <Chat className="text-white h-5 w-5" weight="bold" />
+          </Popover.Trigger>
+        </Popover.Root>
+      </div>
+    </>
   )
 }
